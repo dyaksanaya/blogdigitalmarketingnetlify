fcaa66411d0ce19bc9d822c8b208040b78f6182b
yudaprima <myroofel@gmail.com>
2024-02-01 23:49:25 +0700

    second

---
+++ components/main/footer.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const path = computed(() => route.fullPath.replace('/', ''))
+</script>
+
+<template>
+  <div class="py-5 border-t dark:border-gray-800  mt-5 text-zinc-700 dark:text-zinc-300">
+    <div class="px-6 container max-w-5xl mx-auto">
+      <div class="grid grid-cols-1 md:grid-cols-3">
+        <FooterSite v-if="path === 'about'" />
+        <FooterDevloper v-else />
+
+        <FooterLink />
+        <FooterConnect />
+      </div>
+
+      <div class="border-t dark:border-gray-800 mt-5 text-center p-2">
+        © 2024 🤷‍♂️?
+        <a href="https://roofel.com/pembuatan-website-umkm" target="_blank" rel="dofollow"
+          class="no-underline font-bold">Jasa Pembuatan Website UMKM</a>
+        Roofel.
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.4s;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
---
+++ components/main/header.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const path = computed(() => route.fullPath.replace('/', ''))
+
+const colorMode = useColorMode()
+function onClick(val: string) {
+  colorMode.preference = val
+}
+</script>
+
+<template>
+  <div class="py-5 border-b dark:border-gray-800  font-semibold">
+    <div class="flex px-6 container max-w-5xl justify-between mx-auto items-baseline ">
+      <ul class="flex items-baseline space-x-5">
+        <li class="text-base sm:text-2xl font-bold hidden md:block">
+          <NuxtLink to="/" :class="{ underline: path === '' }">
+            Digital Marketing Corner
+          </NuxtLink>
+        </li>
+      </ul>
+      <ul class="flex items-center space-x-3 sm:space-x-6 text-sm sm:text-lg">
+        <li>
+          <NuxtLink to="/blogs" :class="{ underline: path === 'blogs' }">
+            Blogs
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/categories" :class="{ underline: path === 'categories' }">
+            Categories
+          </NuxtLink>
+        </li>
+        <li title="About Me" :class="{ underline: path === 'about' }">
+          <NuxtLink to="/about" aria-label="About me">
+            About
+          </NuxtLink>
+        </li>
+        <li title="Jasa SEO Specialist" :class="{ underline: path === 'Jasa SEO Specialist' }">
+          <a target="_blank" rel="dofollow" href="https://roofel.com/jasa-seo">Jasa SEO Specialist</a>
+        </li>
+        <li>
+          <ClientOnly>
+            <button v-if="colorMode.value === 'light'" name="light-mode" title="Light"
+              class="hover:scale-110 transition-all ease-out hover:cursor-pointer" @click="onClick('dark')">
+              <Icon name="icon-park:moon" size="20" />
+            </button>
+            <button v-if="colorMode.value === 'dark'" name="dark-mode" title="Dark"
+              class="hover:scale-110 transition-all ease-out hover:cursor-pointer" @click="onClick('light')">
+              <Icon name="noto:sun" size="20" />
+            </button>
+            <template #fallback>
+              <!-- this will be rendered on server side -->
+              <Icon name="svg-spinners:180-ring" size="20" />
+            </template>
+          </ClientOnly>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
